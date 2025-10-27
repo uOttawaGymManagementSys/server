706ae9d8a78d393fee2d7be7b66860fca9e837a5
Oussema Nijewi <oussemasn@gmail.com>
2025-10-27 19:12:36 -0400

    security issue hardcoded passwords and db data removed

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const morgan = require('morgan');
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet);
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json());
@@ -19,15 +19,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 
-const connection = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "WessCha3loma!@",
-    database: "gyminsight"
+const client = new Client({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    port: process.env.PG_PORT,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE
 
 });
 
-connection.connect()
+client.connect()
     .then(() => console.log("connected to database successfully"))
     .catch((err) => console.log(err));
+
+
+/* ROUTES */
+
