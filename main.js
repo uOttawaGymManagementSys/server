398e5e67b5608b741dca394beecdd8e9d1bef679
Oussema Nijewi <oussemasn@gmail.com>
2025-10-30 17:55:42 -0400

    don't remember what this change is

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-const {Client} = require('pg');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const { Client } = require("pg");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const gymEquipmentRoutes = require('./routes/gymEquipmentRoutes.js');
-const gymTrafficRoutes = require('./routes/gymTrafficRoutes.js');
-const dashboardRoutes = require('./routes/dashboardRoutes.js');
+const gymEquipmentRoutes = require("./routes/gymEquipmentRoutes.js");
+const gymTrafficRoutes = require("./routes/gymTrafficRoutes.js");
+const dashboardRoutes = require("./routes/dashboardRoutes.js");
 
 const client = require("./db"); // import the db connection
-
 
 /* Configurations */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/traffic', gymTrafficRoutes);
-app.use('/api/machinestatus', gymEquipmentRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/traffic", gymTrafficRoutes);
+app.use("/api/machinestatus", gymEquipmentRoutes);
 
 /* Listening for requests*/
 const PORT = process.env.SERVER_PORT;
